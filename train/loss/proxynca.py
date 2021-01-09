a52d1973b5eb7535cb494dbe43f4dd4287cadcbb
Im Sunghoon <shi510.kor@gmail.com>
2021-01-09 14:54:44 +0900

    Improve numerical stability
     - log_softmax(x)=x_i-log(sum(exp(x_j)))

--- train/loss/proxynca.py
+++ train/loss/proxynca.py
@@ -23,9 +23,9 @@ def pairwise_distance(A, B):
 
 class ProxyNCALoss(tf.keras.losses.Loss):
 
-    def __init__(self, embedding_dim, classes, scale_x=1, scale_p=3, **kwargs):
+    def __init__(self, n_embedding, n_class, scale_x=1, scale_p=3, **kwargs):
         super(ProxyNCALoss, self).__init__(**kwargs)
-        self.classes = classes
+        self.n_class = n_class
         self.scale_x = scale_x
         self.scale_p = scale_p
         # Training convergence is sometimes slow, starting with low recall rate.
@@ -33,33 +33,28 @@ class ProxyNCALoss(tf.keras.losses.Loss):
         #  - It is better to use orthogonal initializer than random normal initializer.
         self.initializer = tf.keras.initializers.Orthogonal()
         self.proxies = tf.Variable(name='proxies',
-            initial_value=self.initializer((self.classes, embedding_dim)),
+            initial_value=self.initializer((self.n_class, n_embedding)),
             trainable=True)
 
 
     def call(self, y_true, y_pred):
-        binarised = tf.one_hot(y_true, self.classes, True, False)
+        onehot = tf.one_hot(y_true, self.n_class, True, False)
         norm_x = tf.math.l2_normalize(y_pred, axis=1)
-        norm_proxies = tf.math.l2_normalize(self.proxies, axis=1)
+        norm_p = tf.math.l2_normalize(self.proxies, axis=1)
         norm_x = norm_x * self.scale_x
-        norm_proxies = norm_proxies * self.scale_p
-        dist = pairwise_distance(norm_x, norm_proxies)
-        # all distances should be greater than 0.
-        dist = tf.maximum(dist, 0)
-        dist = -1 * dist
+        norm_p = norm_p * self.scale_p
+        dist = pairwise_distance(norm_x, norm_p)
+        dist = -1 * tf.maximum(dist, 0.)
         # for numerical stability,
         # all distances is substracted by its maximum value before exponentiating.
         dist = dist - tf.math.reduce_max(dist, axis=1, keepdims=True)
-        dist = tf.math.exp(dist)
         # select a distance between example and positive proxy.
-        pos = tf.math.reduce_sum(tf.where(binarised, dist, 0), axis=1)
+        pos = tf.where(onehot, dist, 0)
+        pos = tf.math.reduce_sum(pos, axis=1)
         # select all distance summation between example and negative proxy.
-        neg = tf.math.reduce_sum(tf.where(binarised, 0, dist), axis=1)
-        # avoid dividing by zero.
-        neg = tf.maximum(neg, 1e-6)
-        # negative log_softmax.
-        loss = -1 * tf.math.log(pos / neg)
-        # select only positive loss.
-        loss = tf.maximum(loss, 0)
+        neg = tf.where(onehot, 0, tf.math.exp(dist))
+        neg = tf.math.reduce_sum(neg, axis=1)
+        # negative log_softmax: log(exp(a)/sum(exp(b)))=a-log(sum(exp(b)))
+        loss = -1 * (pos - tf.math.log(neg))
         loss = tf.math.reduce_mean(loss)
         return loss
