66c29518c90c0f572e3c242797d98fb69985224b
Im Sunghoon <shi510.kor@gmail.com>
2021-01-28 15:43:01 +0900

    Improve numerical stability of ProxyAnchor Loss

--- train/loss/proxyanchor.py
+++ train/loss/proxyanchor.py
@@ -1,6 +1,21 @@
 import tensorflow as tf
 
 
+@tf.function
+def where_logsumexp(condition, x, axis):
+    """
+    numerical stable logsumexp
+    logsumexp(x) = c + log(sum(x-c)), where c is a maximum along with the axis.
+    """
+    filtered = tf.stop_gradient(
+        tf.where(condition, x, tf.zeros_like(x)))
+    x_max = tf.stop_gradient(
+        tf.math.reduce_max(filtered, axis=axis, keepdims=True))
+    exp = tf.where(condition, tf.exp(x - x_max), tf.zeros_like(x))
+    sumexp = tf.math.reduce_sum(exp, axis=axis, keepdims=True)
+    logsumexp = tf.math.log(sumexp) + x_max
+    return logsumexp
+
 class ProxyAnchorLoss(tf.keras.losses.Loss):
 
     def __init__(self, n_embedding, n_class, scale=30, delta=0.1, **kwargs):
@@ -14,21 +29,19 @@ class ProxyAnchorLoss(tf.keras.losses.Loss):
             trainable=True)
         self.trainable_weights = [self.proxies]
 
-
     def call(self, y_true, y_pred):
-        onehot = tf.one_hot(y_true, self.n_class, 1., 0.)
-        n_positives = tf.math.reduce_sum(onehot)
+        pos_onehot = tf.one_hot(y_true, self.n_class, True, False)
+        neg_onehot = tf.math.logical_not(pos_onehot)
+        num_pos = tf.math.reduce_sum(tf.cast(pos_onehot, tf.float32))
         norm_x = tf.math.l2_normalize(y_pred, axis=1)
         norm_p = tf.math.l2_normalize(self.proxies, axis=1)
         dist = tf.matmul(norm_x, tf.transpose(norm_p))
+        # select all distances between example and positive proxy.
         pos = -1 * self.scale * (dist - self.delta)
+        pos = where_logsumexp(pos_onehot, pos, axis=0)
+        pos = tf.math.reduce_sum(tf.math.softplus(pos)) / num_pos
+        # select all distances between example and negative proxy.
         neg = self.scale * (dist + self.delta)
-        # select a distance between example and positive proxy.
-        pos = tf.where(onehot == 1., pos, 0.)
-        pos = tf.math.reduce_logsumexp(pos, axis=0)
-        pos = tf.math.reduce_sum(tf.math.softplus(pos)) / n_positives
-        # select all distance summation between example and negative proxy.
-        neg = tf.where(onehot == 1., 0., neg)
-        neg = tf.math.reduce_logsumexp(neg, axis=0)
+        neg = where_logsumexp(neg_onehot, neg, axis=0)
         neg = tf.math.reduce_sum(tf.math.softplus(neg)) / self.n_class
         return pos + neg
